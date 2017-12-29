da44f7b2404d51606da769b7f1c9eabde6596dda
Paweł Konik <konius85@gmail.com>
2017-12-29 14:19:00 +0100

    Add Navigation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Router, hashHistory } from 'react-router';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { getCountries } from "./actions/actions-countries";
 import store from './store';
+import routes from './routes';
 import DevTools from './DevTools';
+
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
-            <DevTools />
-        </div>
+            <Router history={hashHistory} routes={routes}>
 
+            </Router>
+            <DevTools/>
+        </div>
     </Provider>,
     document.getElementById('root')
 );
